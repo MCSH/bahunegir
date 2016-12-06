ac2e050ca0e406763b1db71b913ce4503b1bdb7d
Sajjad Heydari <mcshEmail@gmail.com>
2016-12-06 19:39:59 +0330

    added tag support

--- gen.py
+++ gen.py
@@ -1,17 +1,41 @@
 from random import Random
 import time
+import re
+reg = re.compile(r'%([^%]+)%')
 data = []
+misc = {}
+r = Random()
+r.seed(time.time())
 
 with open('excuse.txt','r') as f:
-	data = f.readlines()
+	state = ''
+	for line in f.readlines():
+		if state == '':
+			if line[0] == '%':
+				tag = line[1:-1]
+				if tag in misc:
+					pass
+				else:
+					misc.update({tag:[]})
+				state = tag
+				continue
+			data.append(line[:-1])
+		else:
+			misc.get(state).append(line[:-1])
+
+def replace(a):
+	tag = a.group(1)
+	if tag in misc:
+		d = misc.get(tag)
+		return d[r.randrange(len(d))]
+	return '%'+ tag +'%'
+
 
 def gen(eid=None):
-	r = Random()
 	if eid:
 		r.seed(eid)
-	else:
-		r.seed(time.time())
 	ans = data[r.randrange(len(data))]
+	ans = reg.sub(replace,ans)
 	return ans
 
 if __name__ == '__main__':
