4b6c0e477458ca89d72ba92b8b8354f6e3d09ab8
MCSH <mcshemail@gmail.com>
2016-12-06 20:53:39 +0330

    improved, added farsi data

--- gen.py
+++ gen.py
@@ -8,35 +8,39 @@ r = Random()
 r.seed(time.time())
 
 with open('excuse.txt','r') as f:
-	state = ''
-	for line in f.readlines():
-		if state == '':
-			if line[0] == '%':
-				tag = line[1:-1]
-				if tag in misc:
-					pass
-				else:
-					misc.update({tag:[]})
-				state = tag
-				continue
-			data.append(line[:-1])
-		else:
-			misc.get(state).append(line[:-1])
+    state = ''
+    for line in f.readlines():
+        if line == '\n':
+            continue
+        line = line.replace('\n', '')
+        if line[0] == '%' and line[1] == '%':
+            tag = line[2:]
+            if tag in misc:
+                pass
+            else:
+                misc.update({tag:[]})
+            state = tag
+            continue
+        if state == '':
+            data.append(line)
+        else:
+            misc.get(state).append(line)
 
 def replace(a):
-	tag = a.group(1)
-	if tag in misc:
-		d = misc.get(tag)
-		return d[r.randrange(len(d))]
-	return '%'+ tag +'%'
+    tag = a.group(1)
+    if tag in misc:
+        d = misc.get(tag)
+        return d[r.randrange(len(d))]
+    return '%'+ tag +'%'
 
 
 def gen(eid=None):
-	if eid:
-		r.seed(eid)
-	ans = data[r.randrange(len(data))]
-	ans = reg.sub(replace,ans)
-	return ans
+    if eid:
+        r.seed(eid)
+    ans = data[r.randrange(len(data))]
+    ans = reg.sub(replace,ans)
+    return ans
+
 
 if __name__ == '__main__':
-	print(gen())
+    print(gen())
